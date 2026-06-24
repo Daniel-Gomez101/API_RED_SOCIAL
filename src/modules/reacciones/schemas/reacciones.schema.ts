2dce4950f1cd5175ed381d6a627293d8ff99fe2f
Daniel-Gomez101 <danielgomez12pinzon@gmail.com>
2026-06-24 16:57:35 -0500

    se hizo el reacciones.schema.ts

---
+++ src/modules/reacciones/schemas/reacciones.schema.ts
@@ -0,0 +1,36 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document, Types } from "mongoose";
+
+export type ReaccionDocument = Reaccion & Document;
+
+@Schema({
+    timestamps: true,
+})
+export class Reaccion {
+
+    @Prop({
+        type: Types.ObjectId,
+        ref: "User",
+        required: true,
+    })
+    usuario_id!: Types.ObjectId;
+
+    @Prop({
+        type: Types.ObjectId,
+        ref: "Publicacion",
+        required: true,
+    })
+    publicacion_id!: Types.ObjectId;
+
+    @Prop({
+        required: true,
+    })
+    tipo!: string;
+
+    @Prop({
+        default: true,
+    })
+    activo!: boolean;
+}
+
+export const ReaccionSchema = SchemaFactory.createForClass(Reaccion);
