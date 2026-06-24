bd678cfbb70d2d961b95e0f4241f5c1290f69c90
Daniel-Gomez101 <danielgomez12pinzon@gmail.com>
2026-06-24 17:23:12 -0500

    se corrio app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,24 +7,21 @@ import { RolesModule } from './modules/roles/roles.module';
 import { UsuariosModule } from './modules/usuarios/usuarios.module';
 import { PublicacionesModule } from './modules/publicaciones/publicaciones.module';
 import { ComentariosModule } from './modules/comentarios/comentarios.module';
-import { ReaccionesService } from './modules/reacciones/reacciones.service';
-import { ReaccionesController } from './modules/reacciones/reacciones.controller';
 import { ReaccionesModule } from './modules/reacciones/reacciones.module';
-import { SeguidoresService } from './modules/seguidores/seguidores.service';
 import { SeguidoresModule } from './modules/seguidores/seguidores.module';
 
 @Module({
-    imports: [
-        ConfigModule.forRoot({ isGlobal: true }),
-        MongooseModule.forRoot(process.env.MONGODB_URI as string),
-        RolesModule,
-        UsuariosModule,
-        PublicacionesModule,
-        ComentariosModule,
-        ReaccionesModule,
-        SeguidoresModule,
-    ],
-    controllers: [AppController, ReaccionesController],
-    providers: [AppService, ReaccionesService, SeguidoresService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGODB_URI as string),
+    RolesModule,
+    UsuariosModule,
+    PublicacionesModule,
+    ComentariosModule,
+    ReaccionesModule,
+    SeguidoresModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
