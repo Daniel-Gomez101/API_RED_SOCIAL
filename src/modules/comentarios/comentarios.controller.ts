357ad9f577d231eee57d7d4e6fc53da7c6df4973
Daniel-Gomez101 <danielgomez12pinzon@gmail.com>
2026-06-24 15:41:46 -0500

    se crearon los comentarios.ocntroller y service

--- src/modules/comentarios/comentarios.controller.ts
+++ src/modules/comentarios/comentarios.controller.ts
@@ -1,4 +1,54 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Get, Param, Put, Patch, Delete, Query } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { ComentariosService } from './comentarios.service';
+import { CreateComentarioDto } from './dto/create-comentario.dto';
+import { SearchComentarioDto } from './dto/search-comentario.dto';
+import { UpdateComentarioDto } from './dto/update-comentario.dto';
 
+@ApiTags('Comentarios')
 @Controller('comentarios')
-export class ComentariosController {}
+export class ComentariosController {
+    constructor(
+        private readonly service: ComentariosService,
+    ) {}
+
+    @Post()
+    create(@Body() dto: CreateComentarioDto) {
+        return this.service.create(dto);
+    }
+
+    @Get()
+    findAll(@Query() query: SearchComentarioDto) {
+        return this.service.findAll(query);
+    }
+
+    @Get('inactivos')
+    findInactive() {
+        return this.service.findInactive();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.service.findOne(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() dto: UpdateComentarioDto) {
+        return this.service.update(id, dto);
+    }
+
+    @Patch(':id')
+    updatePartial(@Param('id') id: string, @Body() dto: UpdateComentarioDto) {
+        return this.service.updatePartial(id, dto);
+    }
+
+    @Patch(':id/restore')
+    restore(@Param('id') id: string) {
+        return this.service.restore(id);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: string) {
+        return this.service.remove(id);
+    }
+}
