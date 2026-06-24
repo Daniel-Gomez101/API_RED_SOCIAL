726de53594379c66320b9ad51be612fc8f71ce88
Daniel-Gomez101 <danielgomez12pinzon@gmail.com>
2026-06-24 15:38:03 -0500

    se hizo comentarios.module

--- src/modules/comentarios/comentarios.module.ts
+++ src/modules/comentarios/comentarios.module.ts
@@ -1,4 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ComentariosController } from './comentarios.controller';
+import { ComentariosService } from './comentarios.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Comentario, ComentarioSchema } from './schemas/comentario.schema';
 
-@Module({})
-export class ComentariosModule {}
+@Module({
+    controllers: [ComentariosController],
+    providers: [ComentariosService],
+    imports: [
+        MongooseModule.forFeature([
+            {
+                name: Comentario.name,
+                schema: ComentarioSchema,
+            },
+        ]),
+    ],
+})
+export class ComentariosModule{}
